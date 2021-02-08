728eb66976f82910b5fb3033fcb9d0745485c4ac
mshuvalov <shuvalov99.me@gmail.com>
2021-02-08 15:10:23 +0600

    + Add model

---
+++ TrafficManagementCenter.Server/TrafficManagementCenter.TransportSheduleManager/Model/TransportType.cs
@@ -0,0 +1,9 @@
+﻿namespace TrafficManagementCenter.TransportSheduleManager.Model
+{
+    public class TransportType
+    {
+        public long Key { get; set; }
+        
+        public string Name { get; set; }
+    }
+}
