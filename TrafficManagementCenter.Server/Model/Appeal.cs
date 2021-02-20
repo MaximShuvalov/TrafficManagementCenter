4d2c430ec6822ec0e7d4991816a980671294492c
Shuvalov Max <mshuvalov>
2021-02-20 15:50:25 +0600

    + Model

---
+++ TrafficManagementCenter.Server/Model/Appeal.cs
@@ -0,0 +1,13 @@
+﻿namespace Model
+{
+    public class Appeal
+    {
+        public long Key { get; set; }
+        
+        public string Email { get; set; }
+        
+        public string Fio { get; set; }
+        
+        public string Text { get; set; }
+    }
+}
