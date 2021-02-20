5d7b539759fbdb169021d8f452378d1ea4cccdeb
Shuvalov Max <mshuvalov>
2021-02-20 14:56:44 +0600

    *

---
+++ TrafficManagementCenter.Server/TrafficManagementCenter.Server/Controllers/CitizensAppealsController.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TrafficManagementCenter.Server.Controllers
+{
+    [ApiController]
+    [Route("[api/citizens]")]
+    public class CitizensAppealsController : ControllerBase
+    {
+        // GET
+        public string Index()
+        {
+            return "";
+        }
+
+        [HttpGet("/ping")]
+        public int Ping()
+        {
+            return StatusCodes.Status200OK;
+        }
+    }
+}
