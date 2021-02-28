fe6680afa3ab2df0aec44d9850ac0c89e2586c0d
Shuvalov Max <mshuvalov>
2021-02-28 18:40:59 +0600

    * Изменение модели обращения.

--- TrafficManagementCenter.Server/Model/Appeal.cs
+++ TrafficManagementCenter.Server/Model/Appeal.cs
@@ -3,11 +3,9 @@
     public class Appeal
     {
         public long Key { get; set; }
-        
+        public SubtypeAppeal Subtype { get; set; }
         public string Email { get; set; }
-        
-        public string Fio { get; set; }
-        
         public string Text { get; set; }
+        public string Attachment { get; set; }
     }
 }
