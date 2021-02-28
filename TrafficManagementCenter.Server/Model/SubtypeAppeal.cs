90df6a29f0836d8bad075b39d776c4fbfbaadb04
Шувалов Максим Евгеньевич <mshuvalov@gems.local>
2021-02-28 21:09:33 +0600

    * Подготовил модель для использования в ef core

--- TrafficManagementCenter.Server/Model/SubtypeAppeal.cs
+++ TrafficManagementCenter.Server/Model/SubtypeAppeal.cs
@@ -1,7 +1,10 @@
-﻿namespace Model
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Model
 {
     public class SubtypeAppeal
     {
+        [Key]
         public long Key { get; set; }
         public string Name { get; set; }
         public string Note { get; set; }
--- TrafficManagementCenter.Server/Model/TypeAppeal.cs
+++ TrafficManagementCenter.Server/Model/TypeAppeal.cs
@@ -1,7 +1,10 @@
-﻿namespace Model
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Model
 {
     public class TypeAppeal
     {
+        [Key]
         public int Key { get; set; }
         public string Name { get; set; }
         public string Note { get; set; }
