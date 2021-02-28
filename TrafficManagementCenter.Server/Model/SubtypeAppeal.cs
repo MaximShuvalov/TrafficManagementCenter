572460acefa7ee7fd23381bc145c49d036357ccf
Shuvalov Max <mshuvalov>
2021-02-28 18:33:42 +0600

    + Объекты типа и подтипа обращения.

---
+++ TrafficManagementCenter.Server/Model/SubtypeAppeal.cs
@@ -0,0 +1,10 @@
+﻿namespace Model
+{
+    public class SubtypeAppeal
+    {
+        public long Key { get; set; }
+        public string Name { get; set; }
+        public string Note { get; set; }
+        public TypeAppeal Type { get; set; }
+    }
+}
---
+++ TrafficManagementCenter.Server/Model/TypeAppeal.cs
@@ -0,0 +1,9 @@
+﻿namespace Model
+{
+    public class TypeAppeal
+    {
+        public int Key { get; set; }
+        public string Name { get; set; }
+        public string Note { get; set; }
+    }
+}
