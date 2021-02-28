a0d97002922dab70b2250316dd775f5284495a72
Шувалов Максим Евгеньевич <mshuvalov@gems.local>
2021-02-28 19:06:16 +0600

    * Репозитории для типа и подтипа обращения.

---
+++ TrafficManagementCenter.Server/TrafficManagementCenter.Server.Db/Repositories/SubtypeAppealRepository.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Model;
+
+namespace TrafficManagementCenter.Server.Db.Repositories
+{
+    public class SubtypeAppealRepository : IRepository<SubtypeAppeal>
+    {
+        public SubtypeAppeal Get(long id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<SubtypeAppeal> GetEntities()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task Add(SubtypeAppeal entity)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ TrafficManagementCenter.Server/TrafficManagementCenter.Server.Db/Repositories/TypeAppealRepository.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Model;
+
+namespace TrafficManagementCenter.Server.Db.Repositories
+{
+    public class TypeAppealRepository : IRepository<TypeAppeal>
+    {
+        public TypeAppeal Get(long id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<TypeAppeal> GetEntities()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task Add(TypeAppeal entity)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
