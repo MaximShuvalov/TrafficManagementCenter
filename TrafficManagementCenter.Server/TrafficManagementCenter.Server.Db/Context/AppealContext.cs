f97f1f741bc96a852f02fb25075dea69f598878d
Shuvalov Max <mshuvalov>
2021-03-01 11:58:57 +0600

    + Add Appeal model context

---
+++ TrafficManagementCenter.Server/TrafficManagementCenter.Server.Db/Context/AppealContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Model;
+
+namespace TrafficManagementCenter.Server.Db.Context
+{
+    public class AppealContext : DbContext
+    {
+        public AppealContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Server=localhost; Port=5433; Database=TrafficManagementCenter; Username=TestUser; Password=Qwerty123;");
+        }
+        
+        public DbSet<Appeal> TypeAppeal { get; set; }
+    }
+}
