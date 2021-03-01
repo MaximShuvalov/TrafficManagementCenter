4183ac5f047e7ca57cb2b8c30e7d547af455b429
Shuvalov Max <mshuvalov>
2021-03-01 12:02:53 +0600

    * Fill repository appeal

--- TrafficManagementCenter.Server/TrafficManagementCenter.Server.Db/Repositories/AppealRepository.cs
+++ TrafficManagementCenter.Server/TrafficManagementCenter.Server.Db/Repositories/AppealRepository.cs
@@ -1,25 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Model;
+using TrafficManagementCenter.Server.Db.Context;
 
 namespace TrafficManagementCenter.Server.Db.Repositories
 {
     public class AppealRepository : IRepository<Appeal>
     {
+        private AppealContext _context;
+
         public Appeal Get(long id)
         {
-            throw new System.NotImplementedException();
+            using (_context = new AppealContext())
+                return _context.TypeAppeal.FirstOrDefault(p => p.Key.Equals(id));
         }
 
         public IEnumerable<Appeal> GetEntities()
         {
-            throw new System.NotImplementedException();
+            using (_context = new AppealContext())
+                return _context.TypeAppeal;
         }
 
         public void Add(Appeal entity)
         {
-            throw new System.NotImplementedException();
+            if (entity is null)
+                return;
+            using (_context = new AppealContext())
+            {
+                _context.TypeAppeal.Add(entity);
+                _context.SaveChanges();
+            }
         }
     }
 }
