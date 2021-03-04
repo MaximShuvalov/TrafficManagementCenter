8ac7d8481dc8559b76c7c4bda6e42b4aea88e721
mshuvalov <shuvalov99.me@gmail.com>
2021-03-04 12:08:23 +0600

    * Поправил тип свойства.

--- TrafficManagementCenter.Server/Model/TypeAppeal.cs
+++ TrafficManagementCenter.Server/Model/TypeAppeal.cs
@@ -5,7 +5,7 @@ namespace Model
     public class TypeAppeal
     {
         [Key]
-        public int Key { get; set; }
+        public long Key { get; set; }
         public string Name { get; set; }
         public string Note { get; set; }
     }
