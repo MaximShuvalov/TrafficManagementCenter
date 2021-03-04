4fe8ee6003d20ea58d9c30ba6ec686c8649f995f
Шувалов Максим Евгеньевич <mshuvalov@gems.local>
2021-03-04 22:45:23 +0600

    * Добавил миграцию

--- TrafficManagementCenter.Server/TrafficManagementCenter.Server.Db/Context/AppDbContext.cs
+++ TrafficManagementCenter.Server/TrafficManagementCenter.Server.Db/Context/AppDbContext.cs
@@ -8,18 +8,19 @@ namespace TrafficManagementCenter.Server.Db.Context
         public AppDbContext(DbContextOptions<AppDbContext> options)
             : base(options)
         {
-            Database.EnsureCreated();
+            //Database.EnsureCreated();
         }
 
         public AppDbContext()
         {
-            Database.EnsureCreated();
+            //Database.EnsureDeleted();
+            //Database.EnsureCreated();
         }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseNpgsql(
-                "Server=localhost; Port=5432; Database=TrafficManagementCenter; Username=TestUser; Password=Qwerty123;");
+                "Server=localhost; Port=5433; Database=TrafficManagementCenter; Username=TestUser; Password=Qwerty123;");
         }
 
         public DbSet<Appeal> Appeal { get; set; }
---
+++ TrafficManagementCenter.Server/TrafficManagementCenter.Server.Db/Migrations/20210304164151_InitialCreate.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace TrafficManagementCenter.Server.Db.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TypeAppeal",
+                columns: table => new
+                {
+                    Key = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: true),
+                    Note = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TypeAppeal", x => x.Key);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SubtypeAppeals",
+                columns: table => new
+                {
+                    Key = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: true),
+                    Note = table.Column<string>(type: "text", nullable: true),
+                    TypeKey = table.Column<long>(type: "bigint", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SubtypeAppeals", x => x.Key);
+                    table.ForeignKey(
+                        name: "FK_SubtypeAppeals_TypeAppeal_TypeKey",
+                        column: x => x.TypeKey,
+                        principalTable: "TypeAppeal",
+                        principalColumn: "Key",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Appeal",
+                columns: table => new
+                {
+                    Key = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    SubtypeKey = table.Column<long>(type: "bigint", nullable: true),
+                    Email = table.Column<string>(type: "text", nullable: true),
+                    Text = table.Column<string>(type: "text", nullable: true),
+                    Attachment = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Appeal", x => x.Key);
+                    table.ForeignKey(
+                        name: "FK_Appeal_SubtypeAppeals_SubtypeKey",
+                        column: x => x.SubtypeKey,
+                        principalTable: "SubtypeAppeals",
+                        principalColumn: "Key",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Appeal_SubtypeKey",
+                table: "Appeal",
+                column: "SubtypeKey");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SubtypeAppeals_TypeKey",
+                table: "SubtypeAppeals",
+                column: "TypeKey");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Appeal");
+
+            migrationBuilder.DropTable(
+                name: "SubtypeAppeals");
+
+            migrationBuilder.DropTable(
+                name: "TypeAppeal");
+        }
+    }
+}
