f877545502f4fcd4c3ede18955e54410a97be13b
Шувалов Максим Евгеньевич <mshuvalov@gems.local>
2021-03-05 00:14:07 +0600

    + Фабрика репозиториев

---
+++ TrafficManagementCenter.Server/TrafficManagementCenter.Server.Db/Factory/RepositoryFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Model;
+using TrafficManagementCenter.Server.Db.Context;
+using TrafficManagementCenter.Server.Db.Repositories;
+
+namespace TrafficManagementCenter.Server.Db.Factory
+{
+    // Фабрика репозиториев. Позволяет инкапсулировать создание экземпляров репозиториев.
+    // Таким образом, конкретная реализация интерфейса репозитория создаётся в одном месте
+    // и может быть легко подменена на другую.
+    public static class RepositoryFactory<T>
+    {
+        public static IRepository<T> Create(AppDbContext context)
+        {
+            if (typeof(T) == typeof(Appeal))
+                return new AppealRepository(context) as IRepository<T>;
+            else if (typeof(T) == typeof(TypeAppeal))
+                return new TypeAppealRepository(context) as IRepository<T>;
+            else if (typeof(T) == typeof(SubtypeAppeal))
+                return new SubtypeAppealRepository(context) as IRepository<T>;
+            throw new Exception();
+        }
+    }
+}
