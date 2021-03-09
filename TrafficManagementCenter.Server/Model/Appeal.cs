d817b5b4f2b75faa33630c78ab52f84ec58a8973
mshuvalov <shuvalov99.me@gmail.com>
2021-03-09 10:36:13 +0600

    + Классификация обращения

--- TrafficManagementCenter.Server/Model/Appeal.cs
+++ TrafficManagementCenter.Server/Model/Appeal.cs
@@ -7,6 +7,7 @@ namespace Model
         [Key]
         public long Key { get; set; }
         public SubtypeAppeal Subtype { get; set; }
+        public ClassAppeal ClassAppeal { get; set; }
         public string Email { get; set; }
         public string Text { get; set; }
         public string Attachment { get; set; }
