6f983fb722391f44d36ab55b0fd84359a058f74f
mshuvalov <shuvalov99.me@gmail.com>
2021-03-15 11:01:41 +0600

    + Класс расширений для репозитория подтипа обращения

---
+++ TrafficManagementCenter.Server/TrafficManagementCenter.Server.Db/Extensions/SubtypeAppealRepositoryExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Model;
+using TrafficManagementCenter.Server.Db.Context;
+using TrafficManagementCenter.Server.Db.Repositories;
+
+namespace TrafficManagementCenter.Server.Db.Extensions
+{
+    public static class SubtypeAppealRepositoryExtensions
+    {
+        public static IEnumerable<SubtypeAppeal> GetSubtypeByTypeAppeal(this SubtypeAppealRepository repos,
+            string nameTypeAppeal)
+        {
+            var keyType = new TypeAppealRepository(new AppDbContext()).GetEntities()
+                .FirstOrDefault(p => p.Name.Equals(nameTypeAppeal)).Key;
+            return repos.GetEntities().Where(l => l.TypesId.Equals(keyType));
+        }  
+    }
+}
