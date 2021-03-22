9650dffb481d7fc2a84ac5d8df0ecaeccf484554
AslanTyulegenov <tyulegenov1999@mail.ru>
2021-03-22 16:34:50 +0600

    update script allclasses

--- TrafficManagementCenter.Server/TrafficManagementCenter.UI/wwwroot/js/appeal.jsx
+++ TrafficManagementCenter.Server/TrafficManagementCenter.UI/wwwroot/js/appeal.jsx
@@ -2,35 +2,34 @@ var urlAlltypes = 'http://localhost:8888/api/citizens/alltypes';
 var urlAllclasses  = 'http://localhost:8888/api/citizens/allclasses';
 
 let myFetchAllClasses = fetch(urlAllclasses);
-var myFetchAlltypes = fetch(urlAlltypes);
+let myFetchAllTypes = fetch(urlAlltypes);
 
 myFetchAllClasses.then(function(response) {
   response.text().then(function(text) {
     var sel = document.getElementById("classappeal");
     var options = JSON.parse(text)
 
-for (var i = 0; i<=options.length - 1; i++){
-    console.log(options[i]);
-    var opt = document.createElement('option');
-    opt.value = options[i].key;
-    opt.text = options[i].name;
-    sel.appendChild(opt);
-}
+    for (var i = 0; i<=options.length - 1; i++){
+        console.log(options[i]);
+        var opt = document.createElement('option');
+        opt.value = options[i].key;
+        opt.text = options[i].name;
+        sel.appendChild(opt);
+    }
   });
 });
 
-
-myFetchAlltypes.then(function(response) {
+myFetchAllTypes.then(function(response) {
   response.text().then(function(text) {
     var sel = document.getElementById("typeappeal");
     var options = JSON.parse(text)
 
-for (var i = 0; i<=options.length - 1; i++){
-    console.log(options[i]);
-    var opt = document.createElement('option');
-    opt.value = options[i].key;
-    opt.text = options[i].name;
-    sel.appendChild(opt);
-}
+    for (var i = 0; i<=options.length - 1; i++){
+        console.log(options[i]);
+        var opt = document.createElement('option');
+        opt.value = options[i].key;
+        opt.text = options[i].name;
+        sel.appendChild(opt);
+    }
   });
 });
